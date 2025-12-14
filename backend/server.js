d70cb8f8f2aa7dd7d1671b851604568aec416977
Anzin-Maglente <144773742+AnzinMaglente@users.noreply.github.com>
2025-12-14 11:35:23 +0400

    Added notes and changed styles

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// Import the built-in http module
+const express = require('express');
+const path = require("path");
+
+// Set the hostname and port
+const app = express();
+const port = 3000;
+
+app.engine('html', require('ejs').renderFile); // You can use EJS, Mustache, etc.
+app.set('view engine', 'html');
+app.set("../public", path.join(process.cwd(), "../public"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localHost:${port}`);
+})
